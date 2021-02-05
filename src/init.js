b8660cc61e0db7c05c1ddec854d761fc83add2a9
Kurnia <kurniasuryani@gmail.com>
2021-02-05 01:46:48 +0100

    init

---
+++ src/init.js
@@ -0,0 +1,28 @@
+const TICK_RATE = 3000;
+
+function tick() {
+    console.log("tick", Date.now());
+}
+
+async function init() {
+    console.log("starting game");
+
+    // Function inside of function = closure, persisted time over async fn call
+    let nextTimeToTick = Date.now();
+
+    function nextAnimationFrame() {
+        const now = Date.now();
+        if (nextTimeToTick <= now) {
+            tick();
+            nextTimeToTick = now + TICK_RATE;
+        }
+
+        //request animation is a function in the browser for doing JS animation, 
+        //really useful when browser idle then continue later, instead of set time out to drop out everything
+        requestAnimationFrame(nextAnimationFrame);
+    }
+
+    requestAnimationFrame(nextAnimationFrame); //or nextAnimationFrame();
+}
+
+init();
